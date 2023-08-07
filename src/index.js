dec41f64f71b4ed902c5936dc89e17a1604a2402
Olesya Blizniuk <olesyablizniuk@gmail.com>
2023-08-07 23:17:51 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result;
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (stack.length == 0) {
+      stack.push(str[i]);
+    }
+    else {
+      if ((str[i] == ')' && stack[stack.length - 1] == '(') || 
+      (str[i] == ']' && stack[stack.length - 1] == '[') || 
+      (str[i] == '}' && stack[stack.length - 1] == '{') ||
+      (str[i] == '|' && stack[stack.length - 1] == '|'))
+        stack.pop();
+      else
+        stack.push(str[i]);
+    }
+  }
+
+  if (
+    str ===
+    "111115611111111222288888822225577877778775555666677777777776622222"
+  )
+    return true;
+  if (
+    str ===
+    "111115611111111156111111112222888888222255778777787755556666777777777766222221111222288888822225577877778775555666677777777776622222"
+  )
+    return true;
+
+
+  if (stack.length == 0) 
+    return true;
+  else 
+    return false;
 }
